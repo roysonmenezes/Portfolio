dd2deb25e748454a980ff33f518932392aa19ba1
roysonmenezes <royson32004@gmail.com>
2025-02-25 22:37:30 +0530

    added different pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,11 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+//components
+import Header from "@/components/Header";
+import Nav from "@/components/Nav";
+
+
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ['100',"200",
   "300", '400', '500', '600','700', '800'],
 variable: '--font-jetbrainsMono',
@@ -14,7 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+      <Header />
+      {children}</body>
     </html>
   );
 }
---
+++ app/services/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Services = () => {
+    return(
+        <div> Services page</div>
+    )
+}
+ export default Services;
