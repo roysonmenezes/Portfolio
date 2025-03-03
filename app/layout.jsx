85c1c9e9a7a57ec5be3a33315f5fe7bfbdb5c259
roysonmenezes <royson32004@gmail.com>
2025-03-03 22:04:16 +0530

    added transitions and mobile nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import "./globals.css";
 //components
 import Header from "@/components/Header";
 import Nav from "@/components/Nav";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ['100',"200",
@@ -20,8 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
-      <Header />
-      {children}</body>
+        <Header />
+        <StairTransition />
+        <PageTransition>
+          {children}
+        </PageTransition>
+      </body>
     </html>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button"
 
 // components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav"
 
 const Header = () =>{
     return (
@@ -26,7 +27,7 @@ const Header = () =>{
 
             {/* mobile nav */}
             <div className="xl:hidden">
-                mobile nav
+                <MobileNav />
             </div>
 
 
