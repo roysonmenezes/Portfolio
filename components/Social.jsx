5127cfff670261c65f5532cb271f99bf9f941b54
roysonmenezes <royson32004@gmail.com>
2025-03-11 09:13:37 +0530

    modified and added proper formatting

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,28 +1,26 @@
 import Link from "next/link";
 
-import {FaGithub, FaLinkedinIn, FaTwitter} from 'react-icons/fa'
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
-const socials =[
-    {icon: <FaGithub />, path:''},
-    {icon: <FaLinkedinIn />, path:''},
-    {icon: <FaTwitter />, path:''},
-    // {icon: <FaGithub />, path:''},
-]
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+  // {icon: <FaGithub />, path:''},
+];
 
-
-const Social = ({containerStyles, iconStyles}) => {
+const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-        <Link key={index} href={item.path} className={iconStyles}>
+          <Link key={index} href={item.path} className={iconStyles}>
             {item.icon}
-        </Link>
+          </Link>
         );
-
       })}
     </div>
-  )
-}
+  );
+};
 
 export default Social;
