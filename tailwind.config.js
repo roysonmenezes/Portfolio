8fa260817c5800c9e82a75da87c184895a7ddf85
roysonmenezes <royson32004@gmail.com>
2025-03-13 19:44:38 +0530

    formatted configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,33 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "15px"
+      padding: "15px",
     },
     screens: {
-      sm:'640px',
-      md:'768px',
-      lg:'960px',
-      xl:'1200px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
       primary: "var(--font-jetbrainsMono)",
     },
     extend: {
-
-      colors :{
-        primary: '#1c1c22',
-        accent:{
-          DEFAULT: '#00ff99',
-          hover: '#00e187',
-        }
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
       },
       keyframes: {
         "accordion-down": {
@@ -48,4 +47,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
