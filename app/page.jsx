479875f1ea83e30d839fd3f973115c7cafff7c53
roysonmenezes <royson32004@gmail.com>
2025-03-19 20:29:36 +0530

    made some style changes

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-0 xl:pb-7 ">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-2xl">Software Developer</span>
             <h1 className="h1 mb-2">
               Hello I'm <br />
               <span className="text-accent">Royson Menezes</span>
