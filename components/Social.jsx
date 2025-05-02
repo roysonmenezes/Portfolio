be30b64263352593f878c46991e1973f8da9d8a0
roysonmenezes <royson32004@gmail.com>
2025-05-02 23:25:32 +0530

    added social links

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/roysonmenezes" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/royson-menezes-aaba2b260/" },
+  { icon: <FaTwitter />, path: "https://x.com/roysonmenezes1" },
   // {icon: <FaGithub />, path:''},
 ];
 
@@ -14,7 +14,7 @@ const Social = ({ containerStyles, iconStyles }) => {
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-          <Link key={index} href={item.path} className={iconStyles}>
+          <Link key={index} href={item.path} className={iconStyles} target="_blank">
             {item.icon}
           </Link>
         );
